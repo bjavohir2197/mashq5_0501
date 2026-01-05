c2c26794ed725ba3e6d9615f3ab079580cc0fb57
bjavohir2197 <bjavohir2197@gmail.com>
2026-01-05 12:57:15 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import talaba
+
+
+baholar = [85, 90, 78, 92, 88]
+
+res1 = talaba.ortacha_baho(baholar)
+print(f"Ortacha baho: {res1}")
+
+res2 = talaba.eng_yuqori(baholar)
+print(f"Eng yuqori baho: {res2}")
+
+res3 = talaba.eng_past(baholar)
+print(f"Eng past baho: {res3}")
